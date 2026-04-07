dfe41d23bf86239c2f139814802ad8d67be533e1
M Usman <umalik1019@gmail.com>
2026-04-07 20:01:57 +0500

    Update SocController.java

--- src/main/java/com/soc/siem/controller/SocController.java
+++ src/main/java/com/soc/siem/controller/SocController.java
@@ -1,1 +1,22 @@
+package com.soc.siem.controller;
 
+import com.soc.siem.model.LogEntry;
+import com.soc.siem.repository.LogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/logs")
+@CrossOrigin(origins = "*") // Allows your frontend to talk to the backend
+public class SocController {
+
+    @Autowired
+    private LogRepository logRepository;
+
+    @GetMapping
+    public List<LogEntry> getAllLogs() {
+        return logRepository.findAll();
+    }
+}
