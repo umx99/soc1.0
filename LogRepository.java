c00a590ad9ebcd920011c27e9ab0beeb04ead1af
M Usman <umalik1019@gmail.com>
2026-04-07 20:08:07 +0500

    Delete LogRepository.java

--- LogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.soc.siem.repository;
-
-import com.soc.siem.model.LogEntry;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LogRepository extends JpaRepository<LogEntry, Long> {
-}
